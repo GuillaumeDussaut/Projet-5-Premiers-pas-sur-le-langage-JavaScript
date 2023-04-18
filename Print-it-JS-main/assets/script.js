88de24bb9918a30727fe7ad42c469a01b26282d5
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-04-18 12:28:04 +0200

    maj
    creation du caroussel

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,18 +1,47 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+const slides = document.querySelectorAll('.banner-img');
+const dots = document.querySelectorAll('.dot');
+const precedent = document.querySelector('.arrow_left');
+const suivant = document.querySelector('.arrow_right');
+const nbSlide = slides.length;
+let count = 0;
+let countDots = count;
+
+
+// fleche droite (suivant)
+
+function slideSuivante(){
+	slides[count].classList.remove('active');
+	dots[count].classList.remove('dot_selected');
+
+	if(count < nbSlide - 1){
+		count++;
+	} else {
+		count = 0;
 	}
-]
+
+	slides[count].classList.add('active')
+	dots[count].classList.add('dot_selected')
+}
+suivant.addEventListener('click', slideSuivante)
+
+
+// fleche gauche (precedent)
+
+function slidePrecedente(){
+	slides[count].classList.remove('active');
+	dots[count].classList.remove('dot_selected');
+
+	if(count > 0){
+		count--;
+	} else {
+		count = nbSlide - 1;
+	}
+	
+
+	slides[count].classList.add('active')
+	dots[count].classList.add('dot_selected')
+}
+precedent.addEventListener('click', slidePrecedente)
+
+
+// dots 
