a9750a18abc551cc17668ba2e0c5679b498e5d4f
Guillaume <74982048+GuillaumeDussaut@users.noreply.github.com>
2023-04-20 14:28:36 +0200

    maj

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -2,11 +2,8 @@ const slides = document.querySelectorAll('.banner-img');
 const dots = document.querySelectorAll('.dot');
 const precedent = document.querySelector('.arrow_left');
 const suivant = document.querySelector('.arrow_right');
-const slideDots = document.querySelector('.dot');
 const nbSlide = slides.length;
 let count = 0;
-let countDots = count;
-
 
 // fleche droite (suivant)
 
@@ -25,7 +22,6 @@ function slideSuivante(){
 }
 suivant.addEventListener('click', slideSuivante)
 
-
 // fleche gauche (precedent)
 
 function slidePrecedente(){
@@ -38,15 +34,10 @@ function slidePrecedente(){
 		count = nbSlide - 1;
 	}
 	
-
 	slides[count].classList.add('active')
 	dots[count].classList.add('dot_selected')
 }
 precedent.addEventListener('click', slidePrecedente)
 
-
 // dots 
-function dotSlide(){
-console.log('ok');
-}
-slideDots.addEventListener('click', dotSlide)
+
